013eb4d566303fce91805de33d2208061ed78268
Majid Heidari <majidheidari1997@gmail.com>
2024-04-11 15:34:54 +0330

    init package

---
+++ src/Http/Middleware/CustomMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Majidheidari\SharedModels\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param \Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (request('show_dd') === "true")
+            dd("salam Majid");
+        return $next($request);
+    }
+}
---
+++ src/Providers/SharedModelsProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Majidheidari\SharedModels\Providers;
+
+use App\Http\Kernel;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Support\ServiceProvider;
+use Majidheidari\SharedModels\Http\Middleware\CustomMiddleware;
+
+class SharedModelsProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     * @throws BindingResolutionException
+     */
+    public function boot()
+    {
+        $kernel = $this->app->make(Kernel::class);
+        $kernel->pushMiddleware(CustomMiddleware::class);
+    }
+}
